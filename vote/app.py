9ca65ccddc3b948e0e7500d8ed0b85ea56d856a5
mandali-harish <mandali.harish@gmail.com>
2024-08-11 02:30:33 +0200

    test CICD

--- vote/app.py
+++ vote/app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "Cat")
+option_b = os.getenv('OPTION_B', "Dog")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
